234f06ee1221a93281811c8b79b3a810d7aa417b
Crobe182 <168012702+Crobe182@users.noreply.github.com>
2026-01-28 00:30:05 -0700

    .

--- week4/ponder/responsive.js
+++ week4/ponder/responsive.js
@@ -3,4 +3,17 @@ const navLinks = document.querySelectorAll("nav a");
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("change");
+
+    navLinks.forEach(link => {
+        link.style.display = link.style.display === "block" ? "none" : "block";
+    });
+});
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 700) {
+        menuBtn.classList.remove("change");
+        navLinks.forEach(link => (link.style.display = "inline"));
+    } else {
+        navLinks.forEach(link => (link.style.display = "none"));
+    }
 });

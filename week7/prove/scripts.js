d7dac0c36eef9ff57b582f3da419c546965d0de0
Crobe182 <168012702+Crobe182@users.noreply.github.com>
2026-02-22 02:19:35 -0700

    .

--- week7/prove/scripts.js
+++ week7/prove/scripts.js
@@ -1,11 +1,11 @@
 const form = document.querySelector('#cardForm');
 
 function displayError(msg){
-    document.querySelector('#error').textContent = msg;
+    document.querySelector('.errors').textContent = msg;
 }
 
 function isCardNumberValid(cardNumber){
-    return number === '123412341234';
+    return cardNumber === '1234123412341234';
 }
 
 function submitHandler(event){
@@ -21,9 +21,20 @@ function submitHandler(event){
         errorMsg += 'Card number is invalid.\n';
     }
 
-    const expYear = Number(document.querySelector('#Year').value);
-    const expMonth = Number(document.querySelector('#Month').value);
+    const cardHolder = document.querySelector('#cardHolder').value.trim();
+    if(cardHolder === ''){
+        errorMsg += 'Card holder name is required.\n';
+    } else if (!/^[a-zA-Z\s'-]+$/.test(cardHolder)){
+        errorMsg += 'Please enter a valid name.\n';
+    }
+
+    const expYear = Number(document.querySelector('#year').value);
+    const expMonth = Number(document.querySelector('#month').value);
     const currentDate = new Date();
+
+    if(expMonth < 1 || expMonth > 12){
+        errorMsg += 'Expiration month must be between 1 and 12.\n';
+    }
 
     if(2000 + expYear < currentDate.getFullYear() ||
        (2000 + expYear === currentDate.getFullYear() && expMonth <= currentDate.getMonth() + 1)){
